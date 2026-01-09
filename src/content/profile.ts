a5fce391889ba0638ae2d342d9d431cac9169746
Niwasana <fumiya.tan0214@gmail.com>
2026-01-09 20:16:38 +0900

    Update profile.ts

--- src/content/profile.ts
+++ src/content/profile.ts
@@ -1,6 +1,6 @@
 // === src/content/profile.ts ===
 export const profile = {
-  name: "Amamiya_Ain",
+  name: "Niwasana",
   role: "Kyushu Univ. Grad School M1 / Competitive Programming & Game Dev Enthusiast",
   summary: '堅実な設計と継続的な学びを大切にするエンジニア志望。\n情報系を基盤に、聴覚や自動車分野まで学びを広げつつ、\nCを中心に競技プログラミングやハッカソン、ポートフォリオ制作を通して新技術を実践的に習得しています。',
   links: {
